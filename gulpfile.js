941d0c2dc9c50d38521d44979b3596dfb5598a1e
mirshko <theundeadghost@gmail.com>
2017-01-04 11:40:32 -0500

    Fix issue with files and such

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp 		= require('gulp');
 var browserSync = require('browser-sync').create();
 var sass 		= require('gulp-sass');
-// var uglify 		= require('gulp-uglify');
-// var concat 		= require('gulp-concat');
-var surge 		= require('gulp-surge')
 
 // BROWSER-SYNC
 gulp.task('browser-sync', function() {
@@ -27,30 +24,6 @@ gulp.task('sass', function() {
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
-        .pipe(gulp.dest("./dist/"))
+        .pipe(gulp.dest("./"))
         .pipe(browserSync.stream());
-});
-
-// CONCAT & UGLY JS
-// gulp.task('scripts', function() {
-//     return gulp.src('./js/**/*.js')
-//         .pipe(concat('materialize.js'))
-//         .pipe(uglify())
-//         .pipe(gulp.dest('./dist'));
-// });
-
-// DEPLOY TO SURGE.SH FOR DEVELOPMENT
-gulp.task('deploy-dev', [], function() {
-    return surge({
-        project: './', // PATH TO YOUR STATIC BUILD DIRECTORY
-        domain: 'reiner-io.surge.sh' // YOUR DOMAIN OR SURGE SUBDOMAIN
-    });
-});
-
-// DEPLOY TO SURGE.SH
-gulp.task('deploy', [], function() {
-    return surge({
-        project: './', // PATH TO YOUR STATIC BUILD DIRECTORY
-        domain: 'www.reiner.io' // YOUR DOMAIN OR SURGE SUBDOMAIN
-    });
 });
