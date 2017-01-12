b7737a89ea199ed7c3f9d84b3f7938a52670cff8
mirshko <theundeadghost@gmail.com>
2017-01-12 16:29:22 -0500

    Updating index with external Normalize, minified JS & CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
-var gulp 		= require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass 		= require('gulp-sass');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
 // BROWSER-SYNC
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: './'
         }
     });
 });
 
 // SERVE
 gulp.task('serve', ['sass', 'browser-sync'], function() {
-    gulp.watch("./*.scss", ['sass']);
-    // gulp.watch("./js/**/*.js", ['scripts']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./*.scss', ['sass']);
+    gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
 // COMPILE SASS
 gulp.task('sass', function() {
-    return gulp.src("./*.scss")
+    return gulp.src('./*.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
-        .pipe(gulp.dest("./"))
+        .pipe(rename('master.min.css'))
+        .pipe(gulp.dest('./'))
         .pipe(browserSync.stream());
 });
--- master.js
+++ master.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
     $('input').keyup(function() {
         var empty = false;
         $('input').each(function() {
-            if ($(this).val().length == 0)
+            if ($(this).val().length == 0) {
                 empty = true;
+            }
         });
-        if (empty)
+        if (empty) {
             $('#getThumb').attr('disabled', true);
-        else
+        } else {
             $('#getThumb').attr('disabled', false);
+        }
     });
 });
 
@@ -17,7 +19,8 @@ $(document).ready(function() {
 var subdomain;
 
 // SET BUTTON LISTENER
-document.getElementById("getThumb").addEventListener("click", getThumbnail);
+document.getElementById('getThumb').addEventListener('click', getThumbnail);
+
 function getThumbnail() {
     // GET / SET VALUES FROM INPUT FIELDS
     var mediaHashedId = $('#wistiaID').val();
@@ -25,30 +28,32 @@ function getThumbnail() {
     var height = $('#wistiaThumbHeight').val();
     subdomain = $('#wistiaSubdomain').val();
     // BUILD CALL TO WISTIA
-    var baseUrl = "https://fast.wistia.com/oembed/?url=";
+    var baseUrl = 'https://fast.wistia.com/oembed/?url=';
     // var accountUrl = escape($('#wistiaSubdomain').val().replace('/projects', ''));
-    var accountUrl = escape("http://" + subdomain + ".wistia.com");
+    var accountUrl = escape('http://' + subdomain + '.wistia.com');
     // console.log(accountUrl);
     function getThumbnailUrl(hashedId, callback) {
-        $.getJSON(baseUrl + accountUrl + '/medias/' + mediaHashedId + "&format=json&callback=?", callback);
+        $.getJSON(baseUrl + accountUrl + '/medias/' + mediaHashedId + '&format=json&callback=?', callback);
     }
     // PARSE THE JSON FILE FROM WISTIA
     function parseJSON(json) {
         var thumbnailURL = json.thumbnail_url.replace(/\d+x\d+/, width + 'x' + height);
         // DISPLAY IMAGE THUMBNAIL
         $('#thumbURL').text(thumbnailURL);
-        $('#thumbPreview').attr("src", thumbnailURL);
-        $('#thumbPreview').removeClass("hide");
-        $('#downloadThumb').attr("href", thumbnailURL);
-    };
+        $('#thumbPreview').attr('src', thumbnailURL);
+        $('#thumbPreview').removeClass('hide');
+        $('#downloadThumb').attr('href', thumbnailURL);
+    }
     // CALL THE FUNCTIONS TO GET THE ID FROM WISTIA AND THE INPUT FIELD
     getThumbnailUrl(mediaHashedId, parseJSON);
 }
 // SAVING THE SUBDOMAIN USED
 window.onbeforeunload = function() {
     localStorage.setItem(subdomain, $('#wistiaSubdomain').val());
-}
+};
 window.onload = function() {
     var subdomain = localStorage.getItem(subdomain);
-    if (subdomain !== null) $('#wistiaSubdomain').val(subdomain);
-}
+    if (subdomain !== null) {
+        $('#wistiaSubdomain').val(subdomain);
+    }
+};
