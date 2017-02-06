93fab0b4959c7c2e74712247de63536ba2008517
mirshko <theundeadghost@gmail.com>
2017-02-06 17:59:18 -0500

    Lots of work! Working on adding ability to add play button, loading spinner
    And error handling!

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,48 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
-var surge = require('gulp-surge')
+var surge = require('gulp-surge');
+var uglify = require('gulp-uglify');
 
 // BROWSER-SYNC
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
 });
 
 // SERVE
 gulp.task('serve', ['sass', 'browser-sync'], function() {
-    gulp.watch('./scss/*.scss', ['sass']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+	gulp.watch('./scss/*.scss', ['sass']);
+	gulp.watch('./js/*.js', ['compress']);
+	gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
 // COMPILE SASS
 gulp.task('sass', function() {
-    return gulp.src('./scss/*.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(rename('master.min.css'))
-        .pipe(gulp.dest('./dist/'))
-        .pipe(browserSync.stream());
+	return gulp.src('./scss/*.scss')
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(rename('master.min.css'))
+		.pipe(gulp.dest('./dist/'))
+		.pipe(browserSync.stream());
+});
+
+// UGLIFY SCRIPTS
+gulp.task('compress', function() {
+	return gulp.src('./js/*.js')
+		// .pipe(uglify())
+		.pipe(rename('master.min.js'))
+		.pipe(gulp.dest('./dist/'));
 });
 
 // DEPLOY TO SURGE.SH
 gulp.task('deploy', [], function() {
-    return surge({
-        project: './', // PATH TO YOUR STATIC BUILD DIRECTORY
-        domain: 'thumbnailer.reiner.space' // YOUR DOMAIN OR SURGE SUBDOMAIN
-    });
+	return surge({
+		project: './',
+		domain: 'thumbnailer.reiner.space'
+	});
 });
--- js/master.js
+++ js/master.js
@@ -1,19 +1,20 @@
 // DISABLE BUTTTON WHEN NO INPUT
 $(document).ready(function() {
-    $('input').keyup(function() {
-        var empty = false;
-        $('input').each(function() {
-            if ($(this).val().length == 0) {
-                empty = true;
-            }
-        });
-        if (empty) {
-            $('#getThumb').attr('disabled', true);
-        } else {
-            $('#getThumb').attr('disabled', false);
-        }
-    });
+	$('input').keyup(function() {
+		$('#getThumb').attr('disabled', $('input[required]').toArray().some(function(el) {
+			return el.value.length == 0
+		}));
+	});
 });
+
+var $loading = $('#loading').hide();
+$(document)
+	.ajaxStart(function () {
+		$loading.show();
+	})
+	.ajaxStop(function () {
+		$loading.hide();
+	});
 
 // NEEDED DUE TO SAVING SOME INPUT
 var subdomain;
@@ -22,38 +23,52 @@ var subdomain;
 document.getElementById('getThumb').addEventListener('click', getThumbnail);
 
 function getThumbnail() {
-    // GET / SET VALUES FROM INPUT FIELDS
-    var mediaHashedId = $('#wistiaID').val();
-    var width = $('#wistiaThumbWidth').val();
-    var height = $('#wistiaThumbHeight').val();
-    subdomain = $('#wistiaSubdomain').val();
-    // BUILD CALL TO WISTIA
-    var baseUrl = 'https://fast.wistia.com/oembed/?url=';
-    // var accountUrl = escape($('#wistiaSubdomain').val().replace('/projects', ''));
-    var accountUrl = escape('http://' + subdomain + '.wistia.com');
-    // console.log(accountUrl);
-    function getThumbnailUrl(hashedId, callback) {
-        $.getJSON(baseUrl + accountUrl + '/medias/' + mediaHashedId + '&format=json&callback=?', callback);
-    }
-    // PARSE THE JSON FILE FROM WISTIA
-    function parseJSON(json) {
-        var thumbnailURL = json.thumbnail_url.replace(/\d+x\d+/, width + 'x' + height);
-        // DISPLAY IMAGE THUMBNAIL
-        $('#thumbURL').text(thumbnailURL);
-        $('#thumbPreview').attr('src', thumbnailURL);
-        $('#thumbPreview').removeClass('hide');
-        $('#downloadThumb').attr('href', thumbnailURL);
-    }
-    // CALL THE FUNCTIONS TO GET THE ID FROM WISTIA AND THE INPUT FIELD
-    getThumbnailUrl(mediaHashedId, parseJSON);
+	// GET / SET VALUES FROM INPUT FIELDS
+	var mediaHashedId = $('#wistiaID').val();
+	var width = $('#wistiaThumbWidth').val();
+	var height = $('#wistiaThumbHeight').val();
+
+	var buttonColor = $('#wistiaPlayButtonColor').val();
+
+	subdomain = $('#wistiaSubdomain').val();
+
+	// BUILD CALL TO WISTIA
+	var baseUrl = 'https://fast.wistia.com/oembed/?url=';
+	var accountUrl = escape('http://' + subdomain + '.wistia.com');
+
+	function getThumbnailUrl(hashedId, callback) {
+		$.getJSON(baseUrl + accountUrl + '/medias/' + mediaHashedId + '&format=json&callback=?', callback)
+			.error(function() {
+				alert("Call to Wistia failed, the video ID or account URL may be incorrect. Try again.");
+			});
+	}
+
+	// PARSE THE JSON FILE FROM WISTIA
+	function parseJSON(json) {
+		if ($('#wistiaPlayButton').is(':checked')) {
+			var thumbnailURL = json.thumbnail_url.replace(/\d+x\d+/, width + 'x' + height) + "&image_play_button=true&image_play_button_color=" + buttonColor + "CC"; // CC = 80% Opacity
+		}
+		else {
+			var thumbnailURL = json.thumbnail_url.replace(/\d+x\d+/, width + 'x' + height);
+		}
+
+		// DISPLAY IMAGE THUMBNAIL
+		$('#thumbURL').text(thumbnailURL);
+		$('#thumbPreview').attr('src', thumbnailURL);
+		$('#thumbPreview').removeClass('hide');
+		$('#downloadThumb').attr('href', thumbnailURL);
+	}
+
+	// CALL THE FUNCTIONS TO GET THE ID FROM WISTIA AND THE INPUT FIELD
+	getThumbnailUrl(mediaHashedId, parseJSON);
 }
 // SAVING THE SUBDOMAIN USED
 window.onbeforeunload = function() {
-    localStorage.setItem(subdomain, $('#wistiaSubdomain').val());
+	localStorage.setItem(subdomain, $('#wistiaSubdomain').val());
 };
 window.onload = function() {
-    var subdomain = localStorage.getItem(subdomain);
-    if (subdomain !== null) {
-        $('#wistiaSubdomain').val(subdomain);
-    }
+	var subdomain = localStorage.getItem(subdomain);
+	if (subdomain !== null) {
+		$('#wistiaSubdomain').val(subdomain);
+	}
 };
